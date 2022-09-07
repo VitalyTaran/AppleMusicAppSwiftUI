7442cd60b740d0bfde1868d4499cab5899922fad
VitalyTaran <79220120+VitalyTaran@users.noreply.github.com>
2022-09-07 14:55:51 +0300

    Initial Commit

---
+++ AppleMusicAppHWSwiftUI/AppleMusicAppHWSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusicAppHWSwiftUIApp.swift
+//  AppleMusicAppHWSwiftUI
+//
+//  Created by Виталий Таран on 07.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusicAppHWSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
